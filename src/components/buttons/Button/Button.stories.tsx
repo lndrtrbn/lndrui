1bcd79aaf5d739f4d6d911cfa3ac2c2bd6ebf73f
Landry Trebon <landry.trebon@healthhero.com>
2022-12-22 18:18:58 +0100

    wip test tailwind

--- src/components/buttons/Button/Button.stories.tsx
+++ src/components/buttons/Button/Button.stories.tsx
@@ -8,34 +8,19 @@ export default {
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Primary = Template.bind({})
+Primary.args = {
   children: "Click me!",
 }
 
-export const IconButton = Template.bind({})
-IconButton.args = {
-  children: (
-    <svg width="20" height="10">
-      <rect width="20" height="10" />
-    </svg>
-  ),
-}
-
-export const Transparent = Template.bind({})
-Transparent.args = {
-  ...Default.args,
-  variant: "transparent",
-}
-
-export const Success = Template.bind({})
-Success.args = {
-  ...Default.args,
-  variant: "success",
+export const Secondary = Template.bind({})
+Secondary.args = {
+  ...Primary.args,
+  variant: "secondary",
 }
 
 export const Danger = Template.bind({})
 Danger.args = {
-  ...Default.args,
+  ...Primary.args,
   variant: "danger",
 }
--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -10,7 +10,7 @@ export type ButtonProps = {
   /**
    * The style of the button
    */
-  variant?: "default" | "transparent" | "success" | "danger"
+  variant?: "primary" | "secondary" | "danger"
   /**
    * Callback on button click
    */
@@ -20,16 +20,22 @@ export type ButtonProps = {
 /**
  * Just a simple Button component
  */
-export default function Button({
-  children,
-  variant = "default",
-  onClick,
-}: ButtonProps) {
-  let className = "lndrui lndrui__btn"
-  if (variant !== "default") className += ` lndrui__btn--${variant}`
+export default function Button({ children, variant = "primary", onClick }: ButtonProps) {
+  const classnames = [
+    `bg-${variant}`,
+    `hover:bg-${variant}-dark`,
+    "px-24",
+    "py-8",
+    "rounded",
+    "active:outline",
+    "outline-offset-4",
+    "outline-2",
+    `outline-${variant}-dark"`,
+    "font-bold",
+  ].join(" ")
 
   return (
-    <button className="text-3xl font-bold underline" onClick={onClick}>
+    <button className={classnames} onClick={onClick}>
       {children}
     </button>
   )
